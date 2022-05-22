6e1e5fe61aaea07a158c4e51a970877cc02196f8
raudez77 <43289474+raudez77@users.noreply.github.com>
2022-05-22 22:18:44 +0800

    flaskdone

--- classification_model/for_production.py
+++ classification_model/for_production.py
@@ -1,13 +1,67 @@
 import pandas as pd
+import sys
+sys.path.append(".")
 import numpy as np
+from flask import Flask, request, render_template
+from pydantic import validate_arguments
 from sklearn.model_selection import train_test_split
 from processing.data_manager import data_inputting, DATA_STORE
-from processing.validation import validate_inputs, drop_na_input
+from processing.validation import validate_inputs
+from predict import make_prediction
 from config.core import config
 
-# Inputing / Requestin Data
-data_ = data_inputting(filename=config.app_config.test_data_file)
+# Initiating Flask
+app = Flask(__name__,
+            template_folder='templates',
+            static_folder='templates/static')
 
-# Validating data
-validated_data, errors = validate_inputs(input_data=data_, features_= config.model_config.features)
-print(validated_data)
+
+# Rendering Website
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template("index.html")
+
+
+@app.route('/predict', methods=["POST"])
+def predict():
+    """ Rendering Result on the HTML"""
+    features_ = {
+        'Pclass': None,
+        'Sex': None,
+        'Age': None,
+        'SibSp': None,
+        'Parch': None,
+        'Fare': None,
+        'Embarked': None
+    }
+    if request.method == "POST":
+        for key_ in features_.keys():
+            if key_ not in ['Fare']:
+                if key_ == "Pclass":
+                    if request.form[key_] == '1':
+                        features_["Fare"] = 30
+                    elif request.form[key_] == '2':
+                        features_["Fare"] = 13
+                    else:
+                        features_["Fare"] = 7
+                else:
+                    features_[key_] = request.form[key_]
+
+        # Inputing / Requesting Data
+        data_ = pd.DataFrame(data=features_, index=[0])
+
+        # Load trained - Pipeline
+        predictions = make_prediction(input_data=data_)
+        output_ = np.round(predictions['predictions'][0][1], 4)
+
+        return render_template('predict.html',
+                               prediction_text=output_ * 100,
+                               prediction_class=request.form["Pclass"],
+                               prediction_fare=features_["Fare"])
+    else:
+        pass
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
